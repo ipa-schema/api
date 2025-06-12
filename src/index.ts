3a0a7a74c1c00393a9023482bb652343dbacbf37
leoking <present150608@sina.com>
2025-06-12 17:41:45 +0800

    perf: parseApiResponse

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: leoking
  * @Date: 2025-06-10 21:54:25
- * @LastEditTime: 2025-06-11 00:01:01
+ * @LastEditTime: 2025-06-12 17:40:44
  * @LastEditors: leoking
  * @Description:
  */
@@ -17,7 +17,11 @@ export interface ApiResponse<T> {
   success: boolean;
   error?: ApiError;
 }
-
+/**
+ * 解析接口响应的json/txt数据，如果成功会resolve，如果失败则reject
+ * @param r 响应
+ * @returns promise
+ */
 export async function parseApiResponse<T>(
   r: ApiResponse<T> | Response
 ): Promise<ApiResponse<T>> {
@@ -25,6 +29,18 @@ export async function parseApiResponse<T>(
     return Promise.reject(new Error("unexpected type"));
   }
   if (r instanceof Response) {
+    if (r.status !== 200) {
+      return Promise.reject(new Error(`${r.statusText}`));
+    }
+    const contentType = r.headers.get("Content-Type") ?? "application/json";
+    if (!contentType.endsWith("json")) {
+      return r.text().then((txt) => {
+        return Promise.resolve<ApiResponse<T>>({
+          success: true,
+          data: txt as T,
+        });
+      });
+    }
     return r.json().then((r) => parseApiResponse(r as ApiResponse<T>));
   }
   r = r as ApiResponse<T>;
